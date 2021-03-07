eba1041608a7c0cdfaec31d0c2ced9fb4474bf68
Tip Powell <powelltip@gmail.com>
2021-03-07 01:46:02 -0500

    added tag model and fixed code language attribute so user has to pick it from dropdown menu

--- core/models.py
+++ core/models.py
@@ -1,16 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+Languages = (
+    ('HTML', 'HTML'),
+    ('CSS', 'CSS'),
+    ('JavaScript', 'JavaScript'),
+    ('Python', 'Python'),
+    ('Django', 'Django')
+)
+
 class User(AbstractUser):
     pass
 
 class Snippet(models.Model):
     title = models.CharField(max_length=60)
-    language = models.CharField(max_length=20)
+    language = models.CharField(max_length=20, choices=Languages)
     description = models.CharField(max_length=300)
     script = models.TextField(max_length=1000)
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    tags = models.ManyToManyField('Tag', blank=True)
 
     def __str__(self):
         return self.title
 
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=60, unique=True)
+    language = models.CharField(max_length=20, choices=Languages)
+
+    def __str__(self):
+        return self.tag
